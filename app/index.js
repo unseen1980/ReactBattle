4bc7ff67b9e585664257c1cfdaafc738a46f3cbe
Christos Koutsiaris <koutsiaris@gmail.com>
2016-03-03 17:41:17 +0000

    Added files via upload

--- app/index.js
+++ app/index.js
@@ -1,11 +1,43 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
-var HelloWorld = React.createClass({
-    render: function() {
-        return ( < div >
-            Hello World!
-            < /div>
-        )
-    }
+// var HelloWorld = React.createClass({
+//     render: function() {
+//         return ( < div >
+//             Hello React!
+//             < /div>
+//         )
+//     }
+// });
+// ReactDOM.render( < HelloWorld / > , document.getElementById('app'));
+
+
+var FriendsContainer = React.createClass({
+  render: function(){
+    var name = 'Tyler McGinnis'
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+    return (
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
+      </div>
+    )
+  }
 });
-ReactDOM.render( < HelloWorld / > , document.getElementById('app'));
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render( < FriendsContainer / > , document.getElementById('app'));
